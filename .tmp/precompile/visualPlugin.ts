1aa925fca303bfa726da47f610220bab26d3f9c8
Daniele Perilli <daniele.perilli@gmail.com>
2016-12-20 19:14:27 +0100

    Initial commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_7B952816_A48F_49B4_9E13_15E3BB2C0337_DEBUG = {
+        name: 'PBI_CV_7B952816_A48F_49B4_9E13_15E3BB2C0337_DEBUG',
+        displayName: 'Card with States by OKViz',
+        class: 'Visual',
+        version: '1.3.0',
+        apiVersion: '1.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_7B952816_A48F_49B4_9E13_15E3BB2C0337.Visual(options),
+        custom: true
+    };
+}
